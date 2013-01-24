d4c53f052b08dfe1a7a7c466eaa21d3002b8ea0d
jriquelme <jriquelme@totex.cl>
2013-01-24 11:59:53 -0300

    jrebel spring data plugin activated

--- yarquen-web/src/main/distribution/bin/jrebel-startup.sh
+++ yarquen-web/src/main/distribution/bin/jrebel-startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export JAVA_OPTS="-javaagent:$JREBEL_HOME/jrebel.jar -Drebel.log=true $JAVA_OPTS"
+export JAVA_OPTS="-javaagent:$JREBEL_HOME/jrebel.jar -Drebel.log=true -Drebel.spring_data_plugin=true $JAVA_OPTS"
 `dirname $0`/startup.sh $@
 
